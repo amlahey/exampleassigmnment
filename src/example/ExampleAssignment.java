5fc055a2c1b3b00f4ba6186d1518b66a90de7ceb
Ketan Mayer-Patel <kmp@cs.unc.edu>
2018-08-28 12:59:31 -0400

    Skeleton example assignment.

---
+++ src/example/ExampleAssignment.java
@@ -0,0 +1,16 @@
+package example;
+import java.util.Scanner;
+
+public class ExampleAssignment {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		// Your main program code here.
+		
+	}
+	
+	// Feel free to define addition methods here.
+	// Be sure to declare them as "public static"
+	
+}
